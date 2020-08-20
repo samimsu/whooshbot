9aa17fa97901da33a9a7d8877c6d3d43705e66a4
samimsu <mohammadsamimsu@gmail.com>
2020-08-21 00:38:30 +0500

    edited greet.js

--- commands/greet.js
+++ commands/greet.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'greet',
     description: 'greet',
     execute(message) {
-        message.channel.send('Hello there', message.author);
+        message.channel.send('Hello there', message.author.name);
     },
 };
